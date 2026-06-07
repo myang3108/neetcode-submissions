0d93e7dca70ae1fe2bb76583719087e4f9e42bbf
Michael Yang <myang4035@gmail.com>
2026-06-06 20:33:02 -0700

    Add: maximum-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-subarray/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        # for each element, either start
+        # of new subarray or part of existing one
+
+        # need a currsum and maxsum
+        # if currsum < 0 -> prefix is not helping us -> set to 0
+
+        currsum = 0
+        maxsum = float('-inf')
+        if len(nums) == 1:
+            return nums[0]
+
+        for i in range(len(nums)):
+            currsum += nums[i]
+            maxsum = max(currsum, maxsum)
+            # do it after since we need to first get the maxsum -> first index as an example need -2 at first
+            if currsum < 0:
+                currsum = 0
+
+        return maxsum
