b9ad8325e1984817ca1731af4afb04c80e45ce94
Michael Yang <myang4035@gmail.com>
2026-09-01 15:18:16 -0500

    Add: multiply-strings - submission-2

---
+++ Data Structures & Algorithms/multiply-strings/submission-2.py
@@ -0,0 +1,41 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        # num1 has length m, num2 has length n the answer can have at most: m + n digits
+        res = [0] * (len(num1) + len(num2))
+        # for 123 * 45 -> res = [0,0,0,0,0]
+        # 123 - i
+        # 45  - j
+        # carry = i+j
+        # digit = i + j + 1
+        if num1 == "0" or num2 == "0":
+            return "0"
+
+        # product = digit1 * digit2
+        # total = product + whatever_is_already_there
+
+        # res[currentactualdigitsplace] = total % 10
+        # res[carry] += total // 10
+
+        m = len(num1)
+        n = len(num2)
+
+        res = [0] * (m + n)
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n - 1, -1, -1):
+
+                product = int(num1[i]) * int(num2[j])
+
+                p1 = i + j # carry position
+                p2 = i + j + 1 # actual digit
+
+                total = product + res[p2]
+
+                res[p2] = total % 10 # ones digit
+                res[p1] += total // 10 # carry digit
+
+        # remove leading 0
+        if res[0] == 0:
+            res = res[1:]
+
+        return "".join(map(str, res))
